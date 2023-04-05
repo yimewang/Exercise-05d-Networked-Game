6be46eee741dc63fc6c0564009b27e4ff75a48f5
yimewang <qwerttt7411@gmail.com>
2023-04-05 00:07:29 -0400

    complete exercise

--- Player/Player.gd
+++ Player/Player.gd
@@ -19,14 +19,17 @@ func _physics_process(_delta):
 		velocity.y = 0
 	if velocity != Vector3.ZERO:
 		velocity = move_and_slide(velocity, Vector3.UP)
+		rpc_unreliable("_set_position", global_transform.origin)
 		
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		$Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
 		rotate_y(-event.relative.x * mouse_sensitivity)
+		rpc_unreliable("_set_rotation", rotation.y)
 		$Pivot.rotation.x = clamp($Pivot.rotation.x, -mouse_range, mouse_range)
 
 func die():
+	rpc_unreliable("_die")
 	queue_free()
 
 func get_input():
